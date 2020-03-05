9a8edf9b3ddc5d5e20fc75dd033706221f0b4c13
github username <github email address>
2020-03-05 13:43:52 -0700

    Added complete boulder trail features

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -32,13 +32,16 @@ class Cli
     end
 
     def main_menu
-        choices = ["Find a New Trail to Ride", "View Your Saved Trails", "Browse All Trails",]
+        system("clear")
+        choices = ["Find a New Trail to Ride", "View Your Saved Trails", "Browse All Trails","Quit and Go Ride!"]
         response = prompt.select("What would you like to do today?", choices)
         if (response  == "Find a New Trail to Ride")
             find_trail
         elsif (response == "View Your Saved Trails")
             saved_trails
-        else view_all_trails
+        elsif (response == "Browse All Trails")
+            view_all_trails
+        else
         end
     end
 
@@ -62,10 +65,115 @@ class Cli
 
     def show_boulder_trails
         system("clear")
-        binding.pry
         boulder_trails = Trail.where(location: "Boulder")
         choices = (boulder_trails.map {|trail| trail.name}) << "Back"
         response = prompt.select("Pick a trail.", choices )
-        # if (response == "")
+        if (response == "Betasso Preserve")
+            display_betasso
+        elsif (response == "Walker Ranch")
+            display_walker
+        elsif (response == "Marshall Mesa")
+            display_marshall
+        elsif (response == "West Magnolia")
+            display_magnolia
+        elsif (response == "Heil Valley Ranch")
+            display_heil
+        else view_all_trails
+        end
+    end
+    
+    def display_betasso
+        system("clear")
+        betasso = Trail.all.find_by(name: "Betasso Preserve")
+        
+        puts "#{betasso.name}\nLength: #{betasso.length} Miles\nDifficulty: #{betasso.difficulty}\nStyle: #{betasso.style}"
+        response = prompt.yes?('Would you like to save this trail?')
+        if response == true
+            UserTrail.find_or_create_by(user: session_user, trail: betasso)
+            system("clear")
+            puts "Trail Saved Sucessfully! Now Go Ride!"
+            response = prompt.yes?("...or would you like to view other Boulder Trails now? (nerd)")
+            if response == true
+                show_boulder_trails
+            else main_menu
+            end
+        else show_boulder_trails
+        end
+    end
+
+    def display_walker
+        system("clear")
+        walker = Trail.all.find_by(name: "Walker Ranch")
+        
+        puts "#{walker.name}\nLength: #{walker.length} Miles\nDifficulty: #{walker.difficulty}\nStyle: #{walker.style}"
+        response = prompt.yes?('Would you like to save this trail?')
+        if response == true
+            UserTrail.find_or_create_by(user: session_user, trail: walker)
+            system("clear")
+            puts "Trail Saved Sucessfully! Now Go Ride!"
+            response = prompt.yes?("...or would you like to view other Boulder Trails now? (nerd)")
+            if response == true
+                show_boulder_trails
+            else main_menu
+            end
+        else show_boulder_trails
+        end
+    end
+
+    def display_marshall
+        system("clear")
+        marshall = Trail.all.find_by(name: "Marshall Mesa")
+        
+        puts "#{marshall.name}\nLength: #{marshall.length} Miles\nDifficulty: #{marshall.difficulty}\nStyle: #{marshall.style}"
+        response = prompt.yes?('Would you like to save this trail?')
+        if response == true
+            UserTrail.find_or_create_by(user: session_user, trail: marshall)
+            system("clear")
+            puts "Trail Saved Sucessfully! Now Go Ride!"
+            response = prompt.yes?("...or would you like to view other Boulder Trails now? (nerd)")
+            if response == true
+                show_boulder_trails
+            else main_menu
+            end
+        else show_boulder_trails
+        end
+    end
+
+    def display_magnolia
+        system("clear")
+        magnoliadisplay_magnolia = Trail.all.find_by(name: "West Magnolia")
+        
+        puts "#{magnoliadisplay_magnolia.name}\nLength: #{magnoliadisplay_magnolia.length} Miles\nDifficulty: #{magnoliadisplay_magnolia.difficulty}\nStyle: #{magnoliadisplay_magnolia.style}"
+        response = prompt.yes?('Would you like to save this trail?')
+        if response == true
+            UserTrail.find_or_create_by(user: session_user, trail: magnolia)
+            system("clear")
+            puts "Trail Saved Sucessfully! Now Go Ride!"
+            response = prompt.yes?("...or would you like to view other Boulder Trails now? (nerd)")
+            if response == true
+                show_boulder_trails
+            else main_menu
+            end
+        else show_boulder_trails
+        end
+    end
+
+    def display_heil
+        system("clear")
+        heil = Trail.all.find_by(name: "Heil Valley Ranch")
+        
+        puts "#{heil.name}\nLength: #{heil.length} Miles\nDifficulty: #{heil.difficulty}\nStyle: #{heil.style}"
+        response = prompt.yes?('Would you like to save this trail?')
+        if response == true
+            UserTrail.find_or_create_by(user: session_user, trail: heil)
+            system("clear")
+            puts "Trail Saved Sucessfully! Now Go Ride!"
+            response = prompt.yes?("...or would you like to view other Boulder Trails now? (nerd)")
+            if response == true
+                show_boulder_trails
+            else main_menu
+            end
+        else show_boulder_trails
+        end
     end
 end
