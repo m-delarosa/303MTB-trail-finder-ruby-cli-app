d8059ad5ab7f83def43de742a711f0d3440d7349
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-03-05 09:05:01 -0700

    Add Damon's Cli example

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     has_many :user_trails
     has_many :trails, through: :user_trails
+    validates :username, uniqueness: {case_sensitive: false}
 end
--- runner.rb
+++ runner.rb
@@ -1,6 +1,8 @@
 require_relative 'config/environment'
 
 Cli.new.test
+#or
+Cli.new.main_menu
 
 # binding.pry
 
