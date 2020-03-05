131546badb491a5b27a29c819b5e793f95e78124
github username <github email address>
2020-03-05 11:40:54 -0700

    Add set user, validation, greet and main_menu feature

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
     has_many :user_trails
     has_many :trails, through: :user_trails
-    validates :username, uniqueness: {case_sensitive: false}
+    validates :name, uniqueness: {case_sensitive: false}
 end
